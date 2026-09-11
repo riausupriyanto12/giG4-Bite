4f45dc00cd063fe1ac2414a1560df97c3e9d8948
gus yo <riausupriyanto12@admin.sd.belajar.id>
2026-09-11 15:06:18 +0700

    Perbaiki URL backend v2

--- js/config.js
+++ js/config.js
@@ -1,5 +1,1 @@
-// ============================================================
-// KONFIGURASI — ISI URL /exec DARI DEPLOYMENT GOOGLE APPS SCRIPT
-// ============================================================
-// Cara mendapatkan URL ini: lihat PANDUAN-INSTALASI.md
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbwbj9mZqtm-HeYQ0FhlL_qfA-zHzVaqAlFKUyMUsruRbO8GjTCNmmWC_KkFFgoKfbJ4/exec';
+﻿const GAS_URL = 'https://script.google.com/macros/s/AKfycbwxE3qerz-t4S6KWhL5Hftn31hMlq_cdyNm8mpssSkMAcsv8QIRz4GiFakVgkwN1OSX/exec';
